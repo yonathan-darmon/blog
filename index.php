37620a6a72c543b9b07d5024b661882734cf76d3
yonathan-darmon <yonathan.darmon@laplateforme.io>
2021-12-16 17:42:42 +0100

    debut de travail sur la page index et CSS

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
 <?php
+session_start();
+require "php/classes_user.php";
+require "php/classes-categorie.php";
+require "php/classe_article.php";
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="asset/CSS/header.css">
+    <link rel="stylesheet" href="asset/CSS/index.css">
+    <link rel="icon" type="image/png" href="asset/IMAGE/android-icon-192x192.png">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <?php
+    require "php/header.php"
+    ?>
+</header>
+<main>
+
+</main>
+<footer>
+    <?php
+    require "php/footer.php";
+    ?>
+</footer>
+
+</body>
+</html>
--- php/header.php
+++ php/header.php
@@ -1,43 +1,19 @@
-<?php
-session_start();
-require "classe_article.php";
-require "classes-categorie.php";
-require "classes_user.php";
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../asset/CSS/header.css">
-    <title>Document</title>
-</head>
-<body>
-
-</body>
-</html>
 <nav>
     <ul>
         <li><a href="../index.php">Accueil</a></li>
 
-        <li>
-            <div class="dropdown">
-                <button>Articles</button>
-            <ul>
-                <div class="dropdown-content">
-                    <?php
-                    $catego = new Categorie();
-                    $cat = $catego->getcatego();
-                    for ($i = 0; isset($cat["$i"]); $i++) {
-                        echo '<li><a href="article.php?categorie=' . $cat["$i"]['id'] . '">' . $cat["$i"]['nom'] . '</a>';
-                    }
-                    ?>
-                </div>
-            </ul>
-            </div>
+        <li class="dropdown"><a href="article.php">Articles</a>
 
+
+            <ul class="dropdown-content">
+                <?php
+                $catego = new Categorie();
+                $cat = $catego->getcatego();
+                for ($i = 0; isset($cat["$i"]); $i++) {
+                    echo '<li><a href="article.php?categorie=' . $cat["$i"]['id'] . '">' . $cat["$i"]['nom'] . '</a>';
+                }
+                ?>
+            </ul>
 
         </li>
         <?php
