6fd20050f60c7bcfbf293953758ab939421251f9
yonathan-darmon <yonathan.darmon@laplateforme.io>
2021-12-20 13:17:29 +0100

    Update profil.php

--- php/profil.php
+++ php/profil.php
@@ -3,9 +3,9 @@ session_start();
 require "classes-categorie.php";
 require "classe_article.php";
 require "classes_user.php";
-$info=new User();
-$res4=$info->getAllInfo($_SESSION['login']);
-$res5=$info->getDroits($res4[0]['id_droits']);
+$info = new User();
+$res4 = $info->getAllInfo($_SESSION['login']);
+$res5 = $info->getDroits($res4[0]['id_droits']);
 ?>
 <!doctype html>
 <html lang="en">
@@ -32,10 +32,9 @@ $res5=$info->getDroits($res4[0]['id_droits']);
             echo "<p class='error1'>Veuillez remplir tout les champs!</p>";
         } elseif ($_POST['password'] != $_POST['confirm']) {
             echo "<p class='error1'>Verifiez votre mot de passe</p> ";
-        }
-        else {
+        } else {
             $update = new User();
-            $update->update($_POST['login'],$_POST['password'],$_POST['email']);
+            $update->update($_POST['login'], $_POST['password'], $_POST['email']);
 
 
         }
@@ -46,15 +45,15 @@ $res5=$info->getDroits($res4[0]['id_droits']);
         <img src="../asset/IMAGE/sean.jpg" alt="McVay" class="gauche">
         <form action="#" method="post" class="inscription">
             <label for="login">Nom d'utilisateur</label>
-            <input type="text" name="login" placeholder="<?php echo $res4[0]['login'];?>">
+            <input type="text" name="login" placeholder="<?php echo $res4[0]['login']; ?>">
             <label for="email">Votre Email</label>
-            <input type="text" name="email" placeholder="<?php echo $res4[0]['email'];?>">
+            <input type="text" name="email" placeholder="<?php echo $res4[0]['email']; ?>">
             <label for="password">Mot de passe</label>
             <input type="password" name="password" placeholder="Votre mot de passe">
             <label for="confirm">Confirmation de votre mot de passe</label>
             <input type="password" name="confirm" placeholder=" Votre mot de passe">
             <label for="droits">Vos droits utilisateurs</label>
-            <input type="text" name="droits" value="<?php echo $res5[0]['nom'];?>" readonly>
+            <input type="text" name="droits" value="<?php echo $res5[0]['nom']; ?>" readonly>
             <input type="submit" class="btn" name="submit" value="Modification">
         </form>
     </div>
